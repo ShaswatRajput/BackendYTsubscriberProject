135d493edeae9b12cf6b32663fa2012ca5b9381f
Shaswat <shaswatrajput7@gmail.com>
2024-01-17 19:38:41 +0530

    final

--- src/Routes/users.js
+++ src/Routes/users.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {getAllUsers, getUserById,getUserNames, createNewUser} = require("../controllers/userController.js")
+const {getAllUsers, getUserById,getUserNames} = require("../controllers/userController.js")
 
 //Router Middleware from Express JS
 const router = express.Router()
